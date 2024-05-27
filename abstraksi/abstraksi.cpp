2f6c56078a1406417aebbd8590f23e9f72318bee
Iaannn06 <alfianazad05@gmail.com>
2024-05-27 10:37:47 +0700

    membuat isi dari AbstraksiKlas

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -13,8 +13,14 @@ using namespace std;
 		void display() {
 			cout << "x,= " << x << endl;
 			cout << "y = " << y << endl;
-
 		}
-
 };
+
+	int main() {
+		AbstraksiKlas ak;
+		ak.setXY("Yogyakarta", "Kampus");
+		ak.display();
+
+		return 0;
+	}
 
