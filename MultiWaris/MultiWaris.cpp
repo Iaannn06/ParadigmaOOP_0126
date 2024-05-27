e510e4903048536b421ed5986843c227a156e324
Iaannn06 <alfianazad05@gmail.com>
2024-05-27 10:40:37 +0700

    membuat class untuk umur

---
+++ MultiWaris/MultiWaris.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
+};
+
+
