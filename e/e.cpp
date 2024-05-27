1937ae00cc2a7a204e66cad445455ef6a4d2ebd8
Iaannn06 <alfianazad05@gmail.com>
2024-05-27 09:59:23 +0700

    mmebuat class untuk joko

--- e/e.cpp
+++ e/e.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class seseorag {
+class seseorang {
 public:
 	virtual void pesan() = 0;
 
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
 };
